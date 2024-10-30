07ced22ea15ec7e17b5a686e35b9905243e1f0bf
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-10-30 15:05:12 +0100

    Feat:Mise en place du context pour la bdd

---
+++ todoApp/TodoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using todoApp.Models;
+
+namespace todoApp
+{
+    public class TodoDbContext:DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options) { }
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+    }
+}
